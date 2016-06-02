f5c73bc8338518d088e797fde6378b37de9f0c57
TecDoGt <ddelaguila@tecdogt.com>
2016-06-02 09:11:27 -0600

    Javier menus de botones

---
+++ www/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   
+    $("#button1").click(function(){
+        if (($("#edit1").val() == "123456") && ($("#edit2").val() == "prueba") && ($("#edit3").val() == "password")){
+            document.location="#inicio";
+        }
+    });
+    
+});
