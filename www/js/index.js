b9d1afd370cf7c3ebb69b318d4ffa66d267d5682
TecDoGt <ddelaguila@tecdogt.com>
2016-06-03 09:08:15 -0600

    Deigo fix conflictos

--- www/js/index.js
+++ www/js/index.js
@@ -5,18 +5,7 @@ $(document).ready(function(){
        
     });
     
-<<<<<<< HEAD
-   /* $( "#popuppanel1" ).on({
-=======
-    $( ".popuppanel" ).on({
->>>>>>> ef127aeddce41d6fd59640c4f4cd031a1fb2196b
-        popupbeforeposition: function() {
-            var h = $( window ).height();
 
-            $( ".popuppanel1" ).css( "height", h );
-        }
-        
-    });*/
 
 });
 
