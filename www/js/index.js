a78df5cbae77c287205b10d1ed604a1b22a2597d
TecDoGt <ddelaguila@tecdogt.com>
2016-06-03 10:18:05 -0600

    estebancabreraa wslogin

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
     
+    $("#button1").click(function(){
+        var codigo = $("#edit1").val();
+        var username = $("#edit2").val();
+        var password = $("#edit3").val();
+        $.post("http://tecdogt.com/SentinelHubWS/mediadorApp.php", 
+        {
+            code: codigo, 
+            user: username, 
+            pwd: password
+        }, function(data){
+            if (data.ok == 0){
+                alert("Revise sus datos");
+            } else if (data.ok == 1){
+                window.location = "#inicio";
+                alert("Bienvenido, " + username);
+            }
+        }, "json"); 
+    });
     
 });
-
