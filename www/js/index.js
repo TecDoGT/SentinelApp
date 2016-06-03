41a855c6d7cfa3129e59380672b67fd09669796f
TecDoGt <ddelaguila@tecdogt.com>
2016-06-03 09:50:17 -0600

    estebancabreraa menu actualizado

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
-    $("#button1").click(function(){
-        //if (($("#edit1").val() == "123456") && ($("#edit2").val() == "prueba") && ($("#edit3").val() == "password")){
-            document.location="#inicio";
-       
-    });
     
-
-
+    
 });
 
